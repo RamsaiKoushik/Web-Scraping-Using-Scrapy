cc1c23bbb121319dc16b65485aceacb8e301eff1
RamsaiKoushik <koushikpolisetty@gmail.com>
2023-04-17 04:52:22 +0530

    got a quote

--- quoteTutorial/quoteTutorial/spiders/quote_spider.py
+++ quoteTutorial/quoteTutorial/spiders/quote_spider.py
@@ -8,5 +8,13 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
-        title = response.css('title').extract()
-        yield {'title': title}
+        all_div_quotes = response.css("div.quote")[0]
+        title = all_div_quotes.css("span.text::text").extract()
+        author = all_div_quotes.css(".author::text").extract()
+        tag = all_div_quotes.css(".tag::text").extract()
+        # title = response.css('title').extract()
+        yield {
+            'title': title,
+            'author': author,
+            "tag": tag
+               }
